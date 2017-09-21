fdafa840c4240cbf0b9ef713ebe273a84b630d0a
Michael <mich279q@edu.easj.dk>
2017-09-21 10:29:29 +0200

    Initial commit

---
+++ src/ArealBeregner.java
@@ -0,0 +1,10 @@
+public class ArealBeregner {
+    public static void main(String[] args) {
+        //En integer variabel (r) med tallet som er radius
+        int r = 14;
+        //En variabel der giver udregningen og resultatet
+        double resultat = r * r * 3.14;
+        //Udskriver resultatet
+        System.out.println("Arealet er : " + resultat);
+    }
+}
