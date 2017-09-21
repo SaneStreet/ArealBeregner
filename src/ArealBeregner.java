f3a9b1dac91c101f4ad3918b1faf516f21c0e429
Michael <mich279q@edu.easj.dk>
2017-09-21 10:58:04 +0200

    ArealBeregner med scanner for radius

--- src/ArealBeregner.java
+++ src/ArealBeregner.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class ArealBeregner {
     public static void main(String[] args) {
-        //En integer variabel (r) med tallet som er radius
-        int r = 14;
-        //En variabel der giver udregningen og resultatet
-        double resultat = r * r * 3.14;
+        //Spørger om et tal til Radius
+        System.out.print("Indtast tallet for radius: ");
+        //Scanner for bruger input til radius
+        Scanner input = new Scanner(System.in);
+        //Ny variabel der ideholder radius
+        double radius = input.nextDouble();
+        //udskriver hvad radius er
+        System.out.println("Radius er " + radius);
+        //udregner arealet vha. brugers input af radius
+        double resultat = radius * radius * 3.14;
         //Udskriver resultatet
         System.out.println("Arealet er : " + resultat);
     }
